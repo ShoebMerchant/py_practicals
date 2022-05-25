cb4a1e5299cd613f823d5e16da0679767ac8fcc6
vm-shoeb <shoeb.merchant@purplepatch.in>
2022-05-25 09:31:22 +0530

    complete ass1

--- ass6/aq4.py
+++ ass6/aq4.py
@@ -1,4 +1,5 @@
-# Create a function showEmployee() in such a way that it should accept employee name, and it’s salary and display both, and if the salary is missing in function call it should show it as 9000
+# Create a function showEmployee() in such a way that it should accept employee name,
+# and it’s salary and display both, and if the salary is missing in function call it should show it as 9000
 
 def showEmployee(e_name, e_salary=9000):
     print('Employee name: %s, salary: %s ' % (e_name, e_salary))
